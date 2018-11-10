b4607e7429181d15849dfd819a1925e8af7c1466
Peter <p-binder@hotmail.com>
2018-11-10 15:54:47 -0600

    Added standard footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,47 @@
 <template>
-    <div>
-        <p>This is the footer</p>
-    </div>
+  <v-footer
+    dark
+    height="auto"
+  >
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-3"
+          dark
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-actions class="grey darken-3 justify-center">
+        &copy;2018 — <strong>Vuetify</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram'
+      ]
+    })
+  }
+</script>
