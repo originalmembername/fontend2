ad53fc9509ae157e935f25963f4a95415fa954ab
Peter Binder <p-binder@hotmail.com>
2019-04-26 21:02:02 +0200

    Added simple login & register routine to backend TODO: Connect to frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
 	'corsheaders',
-    'vocab'
+    'vocab',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('api/(?P<version>(v1|v2))/', include('vocab.urls'))
+    re_path('api/(?P<version>(v1|v2))/', include('vocab.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('accounts.urls')) 
 ]
