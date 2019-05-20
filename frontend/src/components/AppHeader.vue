dc6802f090c85e6fb5dd0bb04f879dd6267059f0
Peter Binder <p-binder@hotmail.com>
2019-05-20 16:58:58 +0200

    Added drop-down menu for header links

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,13 +1,19 @@
 <template>
   <v-toolbar>
-    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-menu offset-y content-class="dropdown-menu" transition="slide-y-transition">
+      <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+      <v-card>
+        <v-list>
+          <v-list-tile v-for="(item, index) in menuItems" :key="index">
+            <v-list-tile-title>
+              <router-link :to="item.href" tag="button">{{item.title}}</router-link>
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-card>
+    </v-menu>
     <v-toolbar-title>
-      <v-btn
-      flat
-      :href='&quot;/&quot;'
-      >
-      Language Teaching and Translation
-      </v-btn>
+      <v-btn flat :href="&quot;/&quot;">Language Teaching and Translation</v-btn>
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
@@ -17,3 +23,15 @@
     </v-toolbar-items>
   </v-toolbar>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    menuItems: [
+      { title: "About", href: "/about" },
+      { title: "Login", href: "/login" },
+      { title: "Vocabs", href: "/vocabs" }
+    ]
+  })
+};
+</script>
