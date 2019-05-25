45589126b917848f35169dbc15cb327dc9230975
Peter Binder <p-binder@hotmail.com>
2019-05-25 22:46:48 +0200

    Personal account page that shows first & last name of user

--- backend/authapi/views.py
+++ backend/authapi/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from .authbackend import AuthBackend
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -20,5 +22,8 @@ class AuthView(APIView):
             return JsonResponse({user: 'null'})
 
     def get(self, request):
-        content = {'message': 'This is a secret message for the user'}
+        first_name = request.user.first_name
+        last_name = request.user.last_name
+        content = {'message': 'Welcome user: ' + first_name + " " + last_name}
         return JsonResponse(content)
+
