674418b6d5a442758a243a10375b8fb3945b5b47
Peter Binder <43499205+originalmembername@users.noreply.github.com>
2020-01-01 19:15:29 +0000

    Revert "Removed vuetify (by force), TODO: re-install"
    This reverts commit 7007473ba311488436396d1f20beae325304e717.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import './plugins/axios'
+import './plugins/vuetify'
 import VueResource from 'vue-resource'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import Axios from 'axios'
