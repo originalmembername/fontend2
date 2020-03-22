a4f61bd908bb7dd10d860087e50dffe95da5b6c0
Peter Binder <43499205+originalmembername@users.noreply.github.com>
2020-03-22 19:12:48 +0100

    Created submit & clear button for register form

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,38 +1,44 @@
 <template>
   <v-form v-model="valid">
     <v-container>
-      <v-layout>
-        <v-flex xs12 md4>
-          <v-text-field
-            v-model="username"
-            :rules="nameRules"
-            :counter="10"
-            label="Username"
-            required
-          ></v-text-field>
-        </v-flex>
+      <v-layout column>
+        <v-layout row wrap>
+          <v-flex xs12 md4>
+            <v-text-field
+              v-model="username"
+              :rules="nameRules"
+              :counter="10"
+              label="Username"
+              required
+            ></v-text-field>
+          </v-flex>
 
-        <v-flex xs12 md4>
-          <v-text-field
-            v-model="password"
-            :rules="passwordRules"
-            :counter="10"
-            type="password"
-            label="Password"
-            required
-          ></v-text-field>
-        </v-flex>
-        <v-flex xs12 md4>
-          <v-text-field
-            v-model="confirmPassword"
-            :rules="passwordRules"
-            :counter="10"
-            type="password"
-            label="Confirm Password"
-            required
-          ></v-text-field>
-        </v-flex>
-        <v-btn @click="submit">submit</v-btn>
+          <v-flex xs12 md4>
+            <v-text-field
+              v-model="password"
+              :rules="passwordRules"
+              :counter="10"
+              type="password"
+              label="Password"
+              required
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12 md4>
+            <v-text-field
+              v-model="confirmPassword"
+              :rules="passwordRules"
+              :counter="10"
+              type="password"
+              label="Confirm Password"
+              required
+            ></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-btn @click="submit">submit</v-btn>
+          <v-btn @click="clear">clear</v-btn>
+          <v-alert :value="pwdDifferentAlert" type="error">Passwords don't match!</v-alert>
+        </v-layout>
       </v-layout>
     </v-container>
   </v-form>
@@ -44,7 +50,8 @@ export default {
     valid: false,
     username: "",
     password: "",
-    confirmPassword:"",
+    confirmPassword: "",
+    pwdDifferentAlert: false,
     nameRules: [
       v => !!v || "Name is required",
       v => v.length <= 10 || "Name must be less than 10 characters"
@@ -56,7 +63,17 @@ export default {
   }),
   methods: {
     submit() {
-      this.$v.$touch();
+      if (this.password != this.confirmPassword) {
+        this.pwdDifferentAlert = true;
+        return;
+      }
+    },
+    clear() {
+      this.valid = false;
+      this.username = "";
+      this.password = "";
+      this.confirmPassword = "";
+      this.pwdDifferentAlert = false;
     }
   }
 };
